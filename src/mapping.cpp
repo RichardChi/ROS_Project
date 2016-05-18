a502a82fb509698bdc0596de6971cb7ffc8f9021
RichardChi <lcno.247@hotmail.com>
2016-05-18 23:54:23 +0800

    mapping

---
+++ src/mapping.cpp
@@ -0,0 +1,30 @@
+/*
+*
+*
+*
+*
+* 
+ */
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float32.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <velodyne_pointcloud/point_types.h>
+#include <velodyne_pointcloud/rawdata.h>
+
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/registration/ndt.h>
+#include <pcl/filters/approximate_voxel_grid.h>
+#include <pcl/filters/voxel_grid.h>
