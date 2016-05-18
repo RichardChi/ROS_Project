c1f1694fbb74d170a371518248cb0d1358ad33fc
RichardChi <lcno.247@hotmail.com>
2016-05-19 01:16:51 +0800

    basic framework for mapping

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -86,6 +86,8 @@ static void points_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
     	tf::TransformBroadcaster br;
     	tf::Transform transform;
 
+    	ros::Time scan_time = input->header.stamp;
+
     	pcl::fromROSMsg(*input, tmp);
     	std::vector<int> indices;
    	pcl::removeNaNFromPointCloud(tmp,tmp, indices);
@@ -142,6 +144,103 @@ static void points_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
     	voxel_grid_filter.setLeafSize(voxel_leaf_size, voxel_leaf_size, voxel_leaf_size);
     	voxel_grid_filter.setInputCloud(scan_ptr);
     	voxel_grid_filter.filter(*filtered_scan_ptr);
+
+    	pcl::PointCloud<pcl::PointXYZI>::Ptr map_ptr(new pcl::PointCloud<pcl::PointXYZI>(map));
+
+    	ndt.setTransformationEpsilon(trans_eps);
+    	ndt.setStepSize(step_size);
+    	ndt.setResolution(ndt_res);
+    	ndt.setMaximumIterations(iter);
+    	ndt.setInputSource(filtered_scan_ptr);
+    	ndt.setInputTarget(map_ptr);
+
+    	// pose prediction
+    	// may use EKF function to predict the pose
+    	// MLE function constraint the pose
+    	guess_pos.x = previous_pos.x + offset_x ;
+    	guess_pos.y = previous_pos.y + offset_y;
+    	guess_pos.z = previous_pos.z + offset_z;
+    	guess_pos.roll = previous_pos.roll;
+    	guess_pos.pitch = previous_pos.pitch;
+    	guess_pos.yaw = previous_pos.yaw + offset_yaw;
+    
+    	Eigen::AngleAxisf init_rotation_x(guess_pos.roll, Eigen::Vector3f::UnitX());
+    	Eigen::AngleAxisf init_rotation_y(guess_pos.pitch, Eigen::Vector3f::UnitY());
+    	Eigen::AngleAxisf init_rotation_z(guess_pos.yaw, Eigen::Vector3f::UnitZ());
+    
+    	Eigen::Translation3f init_translation(guess_pos.x, guess_pos.y, guess_pos.z);
+    
+    	Eigen::Matrix4f init_guess = (init_translation * init_rotation_z * init_rotation_y * init_rotation_x).matrix();
+
+    	pcl::PointCloud<pcl::PointXYZI>::Ptr output_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    	ndt.align(*output_cloud, init_guess);
+
+    	t = ndt.getFinalTransformation();
+    
+    	pcl::transformPointCloud(*scan_ptr, *transformed_scan_ptr, t);
+    
+    	tf::Matrix3x3 tf3d;
+    
+    	tf3d.setValue(static_cast<double>(t(0, 0)), static_cast<double>(t(0, 1)), static_cast<double>(t(0, 2)),
+		          static_cast<double>(t(1, 0)), static_cast<double>(t(1, 1)), static_cast<double>(t(1, 2)),
+		          static_cast<double>(t(2, 0)), static_cast<double>(t(2, 1)), static_cast<double>(t(2, 2)));
+ 	   
+    	// Update current_pos.
+    	current_pos.x = t(0, 3);
+    	current_pos.y = t(1, 3);
+    	current_pos.z = t(2, 3);
+    	tf3d.getRPY(current_pos.roll, current_pos.pitch, current_pos.yaw, 1);
+    
+    	transform.setOrigin(tf::Vector3(current_pos.x, current_pos.y, current_pos.z));
+    	q.setRPY(current_pos.roll, current_pos.pitch, current_pos.yaw);
+    	transform.setRotation(q);
+    
+    	br.sendTransform(tf::StampedTransform(transform, scan_time, "map", "base_link"));
+    
+    	// Calculate the offset (curren_pos - previous_pos)
+    	offset_x = current_pos.x - previous_pos.x;
+    	offset_y = current_pos.y - previous_pos.y;
+    	offset_z = current_pos.z - previous_pos.z;
+    	offset_yaw = current_pos.yaw - previous_pos.yaw;
+    
+    	// Update position and posture. current_pos -> previous_pos
+    	previous_pos.x = current_pos.x;
+    	previous_pos.y = current_pos.y;
+    	previous_pos.z = current_pos.z;
+    	previous_pos.roll = current_pos.roll;
+    	previous_pos.pitch = current_pos.pitch;
+    	previous_pos.yaw = current_pos.yaw;
+    
+    	// Calculate the shift between added_pos and current_pos
+    	double shift = sqrt(pow(current_pos.x-added_pos.x, 2.0) + pow(current_pos.y-added_pos.y, 2.0));
+    	if(shift >= SHIFT){
+      		map += *transformed_scan_ptr;
+      		added_pos.x = current_pos.x;
+      		added_pos.y = current_pos.y;
+      		added_pos.z = current_pos.z;
+      		added_pos.roll = current_pos.roll;
+      		added_pos.pitch = current_pos.pitch;
+      		added_pos.yaw = current_pos.yaw;
+    	}
+    
+    	sensor_msgs::PointCloud2::Ptr map_msg_ptr(new sensor_msgs::PointCloud2);
+    	pcl::toROSMsg(*map_ptr, *map_msg_ptr);
+    	*map_msg_ptr.header.frame_id = "map";
+    	*map_msg_ptr.header.stamp = scan_time;
+    	ndt_map_pub.publish(*map_msg_ptr);
+    
+    	q.setRPY(current_pos.roll, current_pos.pitch, current_pos.yaw);
+    	current_pose_msg.header.frame_id = "map";
+    	current_pose_msg.header.stamp = scan_time;
+    	current_pose_msg.pose.position.x = current_pos.x;
+    	current_pose_msg.pose.position.y = current_pos.y;
+    	current_pose_msg.pose.position.z = current_pos.z;
+    	current_pose_msg.pose.orientation.x = q.x();
+    	current_pose_msg.pose.orientation.y = q.y();
+    	current_pose_msg.pose.orientation.z = q.z();
+    	current_pose_msg.pose.orientation.w = q.w();
+    
+    	current_pose_pub.publish(current_pose_msg);
 
 
 }
